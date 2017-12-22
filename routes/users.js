6d2fd338b188bfce16c2619157e96a19f02e9cf9
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2017-12-22 16:26:45 +0700

    Grouping by type

--- routes/users.js
+++ routes/users.js
@@ -12,16 +12,27 @@ var connection = mysql.createConnection({
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  connection.connect();
+
   connection.query('SELECT * from heroes', function (err, rows, fields) {
-  if (err) throw err
-
-  console.log('The solution is: ', rows[0].solution)
-  res.json(rows);
-})
-  connection.end();
-
-
+    if (err) throw err
+    res.json(rows);
+  });
 });
+
+router.get('/group', function(req, res, next){
+  var group = {};
+
+  connection.query('SELECT * from heroes', function (err, rows, fields) {
+    if (err) throw err
+
+    for (var i = 0; i < rows.length; i++) {
+      if (!group[rows[i]["type"]]) {
+        group[rows[i]["type"]] = [];
+      }
+      group[rows[i]["type"]].push(rows[i].name);
+    }
+    res.json(group);
+});
+})
 
 module.exports = router;
